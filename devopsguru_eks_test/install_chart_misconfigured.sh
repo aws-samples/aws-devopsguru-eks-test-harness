9173a1bab06185cd7553e645fde18460a6e739ac
Mirela Popoveniuc <mirelap@amazon.com>
2022-07-07 16:53:08 +0100

    Fix typo from previous commit.

--- devopsguru_eks_test/install_chart_misconfigured.sh
+++ devopsguru_eks_test/install_chart_misconfigured.sh
@@ -6,4 +6,4 @@
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 REGION=$(aws configure get region)
 
-helm install --set image.repository="${AWS_ACCOUNT_ID}".dkr.ecr."${REGINO}".amazonaws.com/devopsguru-eks-test devopsguru-eks-test chart_misconfigured
+helm install --set image.repository="${AWS_ACCOUNT_ID}".dkr.ecr."${REGION}".amazonaws.com/devopsguru-eks-test devopsguru-eks-test chart_misconfigured
